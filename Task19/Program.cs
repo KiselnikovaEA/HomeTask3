63b53fff1d1b05d4859b719b27f56612971f8bec
KisEA <maslennikvakat@mail.ru>
2022-10-26 20:00:06 +0300

    Исправлена функция Palindrom() в 19 задаче

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -7,12 +7,11 @@ namespace HomeTasks_KisEA
         static void Main (string[] args)
         {
             
-            string Palindrom()
+            int Palindrom(int num)
             {
-                Console.Write("Введите неотрицательное пятизначное число: ");
-                int num = Convert.ToInt32(Console.ReadLine());
+
             
-                if ((int)(Math.Log10(num)) != 4) return "Вы ввели не пятизначное число";
+                if ((int)(Math.Log10(num)) != 4) return -1;
                 else{
                     string str = num.ToString();
                     int flag = 0;
@@ -22,18 +21,23 @@ namespace HomeTasks_KisEA
                                                             array[i] = int.Parse(str[i].ToString());
                                                         }
                     for (int i = 0; i < array.Length/2; i ++){
-                                                                if (array[i] == array[array.Length - i - 1]) i++;
-                                                                else {
-                                                                        flag = 1;
-                                                                        break;
-                                                                    }
+                                                                if (array[i] != array[array.Length - i - 1])
+                                                                {
+                                                                    flag = 1;
+                                                                    break;                                                                    
+                                                                }
+                                                                        
                                                             } 
-                    if (flag == 1) return $"Число {num} не является палиндромом";
-                    else return $"Число {num} является палиндромом";
+                    return flag;
+
                 }
             }
             try{
-                    Console.WriteLine(Palindrom());
+                    Console.Write("Введите неотрицательное пятизначное число: ");
+                    int number = Convert.ToInt32(Console.ReadLine());
+                    if (Palindrom(number) == 1) Console.WriteLine($"Число {number} не является палиндромом");
+                    else if (Palindrom(number) == -1) Console.WriteLine("Вы ввели не пятизначное число");
+                    else Console.WriteLine($"Число {number} является палиндромом");
                 }
             catch{
                     Console.WriteLine("Вводить необходимо именно цифры");
